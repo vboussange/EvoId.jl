6d18d42a1785cb45d0e2b9523a246c593d005ad4
Victor Boussange <>
2020-03-30 11:46:58 +0200

    added utils

---
+++ src/ABMEv_utils.jl
@@ -0,0 +1,9 @@
+
+function generalised_gaussian(x::Float64,mu::Float64,sigma::Float64,epsilon::Float64)
+        return exp( -.5 * ((x-mu) / sigma)^epsilon)
+end
+
+"""
+        gaussian(x::Float64,mu::Float64,sigma::Float64) = generalised_gaussian(x,mu,sigma,2)
+"""
+gaussian(x::Float64,mu::Float64,sigma::Float64) = generalised_gaussian(x,mu,sigma,2.)
