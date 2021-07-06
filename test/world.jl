aed35d01c11d173997fd315107686650900fe24e
Victor <bvictor@ethz.ch>
2021-07-06 18:55:25 +0200

    fixed test

--- test/world.jl
+++ test/world.jl
@@ -6,7 +6,7 @@ myspace = (GraphSpace(SimpleGraph(10,10)),RealSpace{1,Float64}())
 myagents = [Agent(myspace,ancestors=true,rates=true) for i in 1:10]
 d(X,Y,t) = gaussian(X[1],Y[1],1)
 b(X,Y,t) = gaussian(X[1],0,1)
-D = [Float32(1),Float64(1.)]
+D = Any[Float32(1),Float64(1.)]
 mu = [1.,1.]
 NMax = 100
 p = Dict{String,Any}();@pack! p = d,b,D,mu,NMax
