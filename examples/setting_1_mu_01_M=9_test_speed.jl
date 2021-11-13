c1a1afb8e9fbd854ea821f5ab69546a8b5b1281b
Victor <bvictor@ethz.ch>
2021-11-13 11:43:42 +0100

    small fix

--- examples/setting_1_mu_01_M=9_test_speed.jl
+++ examples/setting_1_mu_01_M=9_test_speed.jl
@@ -16,21 +16,20 @@ TimeType = Float32 # type for time steps
 
 nodes = Ti(9)
 g = star_graph(nodes)
-dim_neutr = 300
+dim_neutr = 100
 neutralspace = RealSpace{dim_neutr,Tf}()
+myspace = (GraphSpace(g),neutralspace)
 const K1 = Rtype(150)
-# good definition
-@inbounds d(X, Y, t) = (X[1][] ≈ Y[1][]) ? 1f0 / K1 : 0f0
 NMax = 2000
 # tend = Tf(500.)
 tend = TimeType(500.)
 D = [nothing, fill(Tf(5e-2), dim_neutr)]
-b(X,t) = 1f0
-t_saving_cb = collect(range(0, tend, length=300))
-myspace = (GraphSpace(g),neutralspace)
 mu = [Tf(0.1), fill(Tf(1e-1), dim_neutr)]
-p_default = Dict{String,Any}();
-myagents = [Agent(myspace, [[rand(Ti(1):Ti(nodes))], D[2].* randn(Tf,dim_neutr)], Rtype) for i in 1:nodes*Int(K1)]
+b(X,t) = 1f0
+@inbounds d(X, Y, t) = (X[1][] ≈ Y[1][]) ? 1f0 / K1 : 0f0
+myagents = [Agent(myspace, [[rand(Ti(1):Ti(nodes))], D[2].* randn(Tf,dim_neutr)], Rtype) for i in 1:Int(K1)]
+
+myagents = [Agent(myspace, [[rand(Ti(1):Ti(nodes))], D[2].* randn(Tf,dim_neutr)], Rtype) for i in 1:Int(K1)]
 w0 = World(myagents, myspace, D, mu, NMax, t = TimeType(0))
 
 @time sim = run!(w0, Gillepsie(), tend, b, d);
@@ -40,13 +39,17 @@ w0 = World(myagents, myspace, D, mu, NMax, t = TimeType(0))
 #######################
 ####### Plotting ######
 #######################
-if false
-    t_saving_cb = collect(range(0., tend, length=100))
+using PyPlot
+fig, ax = plt.subplots(1,2)
+if true
+    tend = 500.
+    t_saving_cb = collect(range(0., tend, length=200))
     cb(w) = Dict("N" => length(w), "betau" => get_beta_div(w, 2),)
+    myagents = [Agent(myspace, [[rand(Ti(1):Ti(nodes))], D[2].* randn(Tf,dim_neutr)], Rtype) for i in 1:Int(K1)]
+    w0 = World(myagents, myspace, D, mu, NMax, t = TimeType(0))
     @time sim = run!(w0, Gillepsie(), tend, b, d, t_saving_cb=t_saving_cb,cb=cb);
-    using PyPlot
-    fig, ax = plt.subplots(1,2)
     ax[1].plot(get_tspan(sim),sim["N"])
     ax[2].plot(get_tspan(sim),sim["betau"])
+    ax[2].set_yscale("log")
     gcf()
 end
